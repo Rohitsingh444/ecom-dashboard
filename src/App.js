86fe94e08a78379c1344d6e3f01428751e7a7475
Rohitsingh444 <rohitsinghnegi1998@gmail.com>
2021-05-10 15:43:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+     hello rohit
+     <button>Simple</button>
+     <Button>New</Button>
+    </div>
+  );
+}
+
+export default App;
